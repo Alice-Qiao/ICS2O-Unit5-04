1d700c801068e4fbbcda245d1e6f0b121f8de0d2
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-17 21:25:47 +0000

    comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById('image').addEventListener('click', image)
+//the hi button will change activate the image finction
 document.getElementById('text').addEventListener('click', change)
+//when the text button is clicked, the change function will be activated
 document.getElementById('colour').addEventListener('click', color)
-//when the hi button is pressed, the function image will activate. when the what button is pressed, the change funtion will activate.
+//when the click me button is clicked, it will activate the color function
 
 function image () {
   document.getElementById('food').src ='Images/paimon.jpg'
@@ -12,8 +14,9 @@ function change () {
   document.getElementById('hi').style.display = 'none'
   document.getElementById('paimon').style.display = 'block'
   }
-  //when this function is acitvated, the styles will be changed in css. the hello statemant will be hidden and the paimon statement will be shown.
+  //when this function is acitvated, the styles will be changed in css. the hello statement will be hidden and the paimon statement will be shown.
 
 function color () {
   document.body.style.backgroundColor = 'tomato'
 }
+//the function will change the background color when activated by changing the style in css
